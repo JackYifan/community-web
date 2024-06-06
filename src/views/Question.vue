9d8e201e23f2daf06e6219beca5c772ad8a6b218
JackYifan <1779626055@qq.com>
2024-06-06 21:04:51 +0800

    feat: add question page

--- src/views/Question.vue
+++ src/views/Question.vue
@@ -1,20 +1,61 @@
 <script>
+import service from "@/util/request";
+import moment from "moment/moment";
+
 export default {
   name: "question",
   data() {
     return {
-      id: 1
-
+      id: 1,
+      question:{},
     };
   },
   mounted() {
     this.id = this.$route.params.id;
+    this.getQuestionInfo(this.id);
+
+  },
+  methods:{
+    getQuestionInfo(id) {
+      service.get(`/question/${id}`)
+          .then((res) => {
+            this.question = res.data
+          })
+    },
+    convertGMT(val){
+      return moment(val).format('YYYY-MM-DD HH:mm:ss')
+    }
   }
 }
 </script>
 
 <template>
-<h1>Question{{id}}</h1>
+  <div class="main profile">
+    <h4 class="question-title"><span v-text="question.title"></span></h4>
+    <span class="text-desc">
+      作者：<span v-text="question.user.name"></span> |
+      发布时间：<span v-text="`${convertGMT(question.gmtCreate)}`"></span> |
+      阅读数： <span v-text="question.viewCount"></span>
+    </span>
+    <a-row>
+      <a-col :span="18">
+        <div>
+          <mavon-editor
+              :value="question.description"
+              :subfield="false"
+              defaultOpen="preview"
+              :toolbarsFlag="false"
+              boxShadowStyle="0"
+              previewBackground="#fff"
+              codeStyle="obsidian"
+              :xssOptions=false></mavon-editor>
+        </div>
+      </a-col>
+    </a-row>
+
+  </div>
+
+
 </template>
 
 <style scoped>
